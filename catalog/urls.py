60b5ee24829727156e0261fb071818e738def766
Evg Fomin <efominykhgh@gmail.com>
2025-12-24 20:32:17 +0300

    fix: update catalog urls to use CBV .as_view()

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from . import views
 
-app_name = 'catalog'
+from .views import (AddProductView, ContactsView, HomeView, ProductDeleteView,
+                    ProductDetailView, ProductUpdateView)
+
+app_name = "catalog"
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('products/<int:pk>/', views.product_detail, name='product_detail'),
-    path('add-product/', views.add_product, name='add_product'),
+    path("", HomeView.as_view(), name="home"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("add-product/", AddProductView.as_view(), name="add_product"),
+    path(
+        "products/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"
+    ),
+    path(
+        "products/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"
+    ),
 ]
